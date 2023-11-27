f15851da0960ca751ff129a9a216c8f5a0f6638b
TheGuyInTheShell <111715094+TheGuyInTheShell@users.noreply.github.com>
2023-11-27 19:39:55 -0400

    Improved: typed

--- database/utils/get_pagination_by_model.py
+++ database/utils/get_pagination_by_model.py
@@ -1,11 +1,11 @@
-from sqlalchemy import select, desc
+from sqlalchemy import Select, select, desc
 from database import BaseAsync, BaseSync
 
-async def get_pagination_by_model(pag: int, ord: int, Model: BaseAsync | BaseSync) -> str:
+def get_pagination_by_model(pag: int, ord: int, Model: BaseAsync | BaseSync) -> Select[any]:
     offset = (pag - 1)*10
     limit = 10
     if ord == 'DESC': 
         query = select(Model).order_by(desc(Model.id)).offset(offset).limit(limit)
     else:
         query = select(Model).offset(offset).limit(limit)
-    return await query
+    return query
