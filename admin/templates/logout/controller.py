6e130f5c875589afe8cab1d1522dfa2c1dcacb8f
TheGuyInTheShell <111715094+TheGuyInTheShell@users.noreply.github.com>
2025-12-09 18:32:16 -0400

    Add logout functionality with a dedicated endpoint and integrate it into the admin navbar.

---
+++ admin/templates/logout/controller.py
@@ -0,0 +1,18 @@
+from fastapi import Request
+from fastapi.responses import RedirectResponse
+from fastapi.routing import APIRouter
+from fastapi.templating import Jinja2Templates
+
+class InitTemplate:
+    def __init__(self, templates: Jinja2Templates):
+        self.templates = templates
+        self.router = APIRouter()
+
+    def add_all(self) -> APIRouter:
+        @self.router.get("")
+        async def logout(request: Request):
+            response = RedirectResponse(url="/admin/sign-in", status_code=302)
+            response.delete_cookie(key="access_token")
+            return response
+            
+        return self.router
